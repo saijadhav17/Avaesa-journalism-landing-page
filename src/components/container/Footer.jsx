ed35ad74745f07e068def74b5ef6cccea932c3f9
saijadhav17 <saijadhav1723@gmail.com>
2024-03-15 18:32:53 +0530

    final

--- src/components/container/Footer.jsx
+++ src/components/container/Footer.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BsFacebook, BsInstagram, BsTwitter, BsPinterest } from "react-icons/bs";
+import {
+  BsFacebook,
+  BsInstagram,
+  BsTwitter,
+  BsPinterest,
+} from "react-icons/bs";
 import { motion } from "framer-motion";
 
 const Footer = () => {
@@ -15,7 +20,8 @@ const Footer = () => {
         <div>
           <div className="font-bold mb-6">Get Started</div>
           <p className="text-sm leading-7">
-            Enroll now and unlock journalism mastery! Join Mr. Arun's course to embark on your storytelling journey today.
+            Enroll now and unlock journalism mastery! Join Mr. Arun's course to
+            embark on your storytelling journey today.
           </p>
         </div>
 
@@ -23,10 +29,18 @@ const Footer = () => {
         <div>
           <div className="font-bold mb-6">Services</div>
           <div className="flex flex-col gap-4">
-            <a href="#" className="text-sm hover:underline">Online Courses</a>
-            <a href="#" className="text-sm hover:underline">Personalized Mentoring</a>
-            <a href="#" className="text-sm hover:underline">Workshops and Webinars</a>
-            <a href="#" className="text-sm hover:underline">Networking</a>
+            <a href="#" className="text-sm hover:underline">
+              Online Courses
+            </a>
+            <a href="#" className="text-sm hover:underline">
+              Personalized Mentoring
+            </a>
+            <a href="#" className="text-sm hover:underline">
+              Workshops and Webinars
+            </a>
+            <a href="#" className="text-sm hover:underline">
+              Networking
+            </a>
           </div>
         </div>
 
@@ -34,24 +48,48 @@ const Footer = () => {
         <div>
           <div className="font-bold mb-6">Resources</div>
           <div className="flex flex-col gap-4">
-            <a href="#" className="text-sm hover:underline">Journalism Guides</a>
-            <a href="#" className="text-sm hover:underline">Writing Tips</a>
-            <a href="#" className="text-sm hover:underline">Career Advice</a>
-            <a href="#" className="text-sm hover:underline">Tools & Software</a>
+            <a href="#" className="text-sm hover:underline">
+              Journalism Guides
+            </a>
+            <a href="#" className="text-sm hover:underline">
+              Writing Tips
+            </a>
+            <a href="#" className="text-sm hover:underline">
+              Career Advice
+            </a>
+            <a href="#" className="text-sm hover:underline">
+              Tools & Software
+            </a>
           </div>
         </div>
 
-        {/* Follow Us */}
+        {/* Connect Us */}
         <div>
           <div className="font-bold mb-6">Connect Us</div>
-          <div className="text-sm mb-4">Insightfullearning@gmail.com</div>
-          <div className="text-sm mb-4">arunlearning@gmail.com</div>
-          <div className="text-sm">+959883271929</div>
-          <div className="flex gap-4 mt-4">
-            <a href="#" className="hover:scale-110 text-xl"><BsFacebook /></a>
-            <a href="#" className="hover:scale-110 text-xl"><BsInstagram /></a>
-            <a href="#" className="hover:scale-110 text-xl"><BsTwitter /></a>
-            <a href="#" className="hover:scale-110 text-xl"><BsPinterest /></a>
+          <div className="flex flex-col gap-4">
+            <a href="#" className="text-sm hover:underline cursor-pointer">
+              Insightfullearning@gmail.com
+            </a>
+            <a href="#" className="text-sm hover:underline cursor-pointer">
+              arunlearning@gmail.com
+            </a>
+            <a href="#" className="text-sm hover:underline cursor-pointer">
+              +919898989978
+            </a>
+          </div>
+          <div className="flex gap-4 mt-4 ">
+            <a href="#" className="hover:scale-110 text-xl">
+              <BsFacebook />
+            </a>
+            <a href="#" className="hover:scale-110 text-xl">
+              <BsInstagram />
+            </a>
+            <a href="#" className="hover:scale-110 text-xl">
+              <BsTwitter />
+            </a>
+            <a href="#" className="hover:scale-110 text-xl">
+              <BsPinterest />
+            </a>
           </div>
         </div>
       </div>
